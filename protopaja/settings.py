8a51271bd3f59acaaa8b1383d03efaf32d120f81
lauri.westerholm <lauri.westerholm@aalto.fi>
2018-08-10 15:00:50 +0300

    Small change

--- protopaja/settings.py
+++ protopaja/settings.py
@@ -128,5 +128,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'protoryhma3@gmail.com'
-EMAIL_HOST_PASSWORD = 'Protopaja_ryhma3'
+EMAIL_HOST_PASSWORD = 'passwd'
 EMAIL_PORT = 587
