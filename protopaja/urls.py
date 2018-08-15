87cdc4e3b4f2201f62ddac3d07d5f6044a2c581b
Vikke Tiirola <vikke.tiirola@gmail.com>
2018-08-15 12:08:27 +0300

    Reverted url change. Some adjustments to style

--- protopaja/urls.py
+++ protopaja/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-	path('protopaja/', include('devicedata.urls')),
+	path('', include('devicedata.urls')),
 	path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
